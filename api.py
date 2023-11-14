9efe8578909cd5c961263003d6a595eb9a18449f
SpikeBonjour <triphuong@cbnteck.org>
2023-11-15 00:19:51 +0700

    big brein hack

--- api.py
+++ api.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from multiprocessing import Process
+import main
+
 
 app = FastAPI()
 
@@ -6,3 +9,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"code": "500", "message": "API not yet available!"}
+
+
+proc = Process(
+    target=main.main,
+    daemon=True,
+)
+proc.start()
